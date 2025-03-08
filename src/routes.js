3043e58e2f11417d465c4ca036afd24c6a200863
Guilherme Felix <f3lixguilherme@gmail.com>
2025-03-08 12:04:12 -0300

    conexeão com banco de dados em JSON local, com persistência

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,33 @@
-// export const routes = [
-//     {
-//         //criar task
-//         method: 'POST',
-//         path: '/tasks',
-//         handler:(req, res)=>{
-            
-//         },
-//     },
-// ];
+import { randomUUID } from "node:crypto"
+import { Database } from "../database.js"
+
+const arrayTasks= []
+const database = new Database
+export const routes = [
+    {
+        method:'GET',
+        url: '/tasks',
+        handler:(req, res)=>{
+            const tasks = database.metodoSelect('tasks')
+
+            return res.end(JSON.stringify(tasks))
+        }
+    },
+    {
+        method:'POST',
+        url: '/tasks',
+        handler:(req, res)=>{
+            const {title, description} = req.body
+            const newTask = {
+                id: randomUUID(),
+                title,
+                description,
+                completed_at: null,
+                created_at: new Date().toISOString().split('T')[0],
+                updated_at: null,
+            }
+            database.metodoInsert('tasks',newTask)
+            return res.writeHead(201).end()
+        }
+    },
+]
--- src/server.js
+++ src/server.js
@@ -1,33 +1,28 @@
-import { randomUUID } from 'node:crypto'
+
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
 
-const arrayTasks= []
+
 
 const server = http.createServer(async(req, res)=>{
     const {method, url} = req //daqui eu retiro o metodo e o caminho da requisicao
     
     await json(req,res)
 
-    if(method === "GET" && url === "/tasks"){
+    const rota = routes.find(rota=>{
+        return rota.method === method && rota.url === url
+    })
+
+    if(rota){
+        return rota.handler(req,res)
+    }
+
+    // if(method === "POST" && url === "/tasks"){
         
-        return res.end(JSON.stringify(arrayTasks))
-    }
-
-    if(method === "POST" && url === "/tasks"){
-        const task = {
-            id: randomUUID(),
-            title: "Tarefa teste",
-            description: "uma tarefa imaginária pra testar",
-            completed_at: null,
-            created_at: new Date().toISOString().split('T')[0],
-            updated_at: null,
-        }
-
-        arrayTasks.push(task)
-        return res.writeHead(201).end('Task Adicionada')
-    }
-    return res.end('Ola mundo!')
+    //     return res.writeHead(201).end('Task Adicionada')
+    // }
+    // return res.end('Ola mundo!')
 })
 
 server.listen(3030)
